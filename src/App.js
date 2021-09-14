923cf7f6483a851b37c1afc31f3f38e3c0f1e440
Bruce Qin <qinfeng21@hotmail.com>
2021-09-14 07:37:54 -0700

    adding clear button

--- src/App.js
+++ src/App.js
@@ -6,44 +6,55 @@ import axios from 'axios';
 import './App.css';
 
 class App extends Component {
-
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null,
   };
-  async componentDidMount() {
+  // async componentDidMount() {
 
+  //   this.setState({ loading: true });
+  //   const res = await axios.get(`https://api.github.com/users?client_id=
+  //   ${process.env.REACT_APP_GITHUB_CLIENT_ID} & clent_secret=
+  //   ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+  //   this.setState({ users: res.data, loading: false });
+
+  // }
+  searchUsers = async (text) => {
     this.setState({ loading: true });
-    const res = await axios.get(`https://api.github.com/users?client_id=
-    ${process.env.REACT_APP_GITHUB_CLIENT_ID} & clent_secret=
-    ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
-
-    this.setState({ users: res.data, loading: false });
-
-  }
-  searchUsers = async text => {
-    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=
+    const res =
+      await axios.get(`https://api.github.com/search/users?q=${text}&client_id=
     ${process.env.REACT_APP_GITHUB_CLIENT_ID} & clent_secret=
     ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
     this.setState({ users: res.data.items, loading: false });
   };
 
-  render() {
+  clearUsers = () => this.setState({ users: [], loading: false });
 
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+  };
+
+  render() {
+    const { users, loading } = this.state;
 
     return (
       <div className="App">
-        <Navbar title='Github Finder' icon='fab fa-github' />
+        <Navbar title="Github Finder" icon="fab fa-github" />
         <div className="container">
-          <Search searchUsers={this.searchUsers} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
+          />
 
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
   }
-
 }
 
 export default App;
